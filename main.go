39b41e3900626339bd67a3b656408d197cde0b58
nmyshenkov <nikita11e@gmail.com>
2019-03-11 00:23:32 +0300

    server

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"log"
+)
+
+type Message struct {
+	From string
+	To   string
+	Body string
+}
+
+func (m *Message) ToString() string {
+	return m.From + ": " + m.Body
+}
+
+type Server struct {
+	messages  []Message
+	sendMsgCh chan *Message
+	doneCh    chan bool
+	errCh     chan error
+}
+
+func (s *Server) Start() {
+	for {
+		select {
+		case err := <-s.errCh:
+			log.Println("Error:", err.Error())
+
+		case <-s.doneCh:
+			return
+		}
+	}
+}
+
 func main() {
 
 }
